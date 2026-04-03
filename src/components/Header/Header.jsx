a5e6e850b0d37042f9999bfd6c4cddff5fe1d4d7
sanju <sanjuk0064@gmail.com>
2026-04-03 21:40:00 +0530

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,6 +47,8 @@ const Header = ({ onSidebarToggle, websiteMode }) => {
 
       "/users": "Users Management > Users",
 
+      "/leave-request":"All Request > Leave Request",
+
       "/exam-grade-system": "Exam Grade System",
       "/subjects": "Subjects",
       "/languages": "Languages",
